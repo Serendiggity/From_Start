34d0ff4d0fa1a031b9d70cd72bc219899befe019
Serendiggity <73052302+Serendiggity@users.noreply.github.com>
2020-12-06 21:20:09 -0700

    Create Kosi 1.py

---
+++ Kosi 1.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+print("Hello","World", 42, 2+2)
+
+print("My first name is DJ")
+print("\n")
+print("My last name is Erinphant")
+print("My faculty is Science")
+print("My year is One")
+
+print("     OOOOOOOOOOOO")
+print("OOOOOOOOOOOO")

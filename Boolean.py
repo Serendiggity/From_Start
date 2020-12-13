76e4acf762c94743b329a3b2579bb8e12ca668b6
Serendiggity <73052302+Serendiggity@users.noreply.github.com>
2020-12-12 22:50:24 -0700

    Create Boolean.py

---
+++ Boolean.py
@@ -0,0 +1,13 @@
+a=1
+if a==1:
+    print("a is 1")
+
+grade = 94
+
+if grade >= 50:
+    print ("you passed")
+    print("congratulations!")
+
+if grade < 50:
+    print("the grade is below 50")
+    print("i'm sorry.")

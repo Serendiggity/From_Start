eb98c7394922b03448c1f075e1046805dabb7d37
Serendiggity <73052302+Serendiggity@users.noreply.github.com>
2020-12-17 11:25:36 -0700

    Create 4.2 If_Else.py

---
+++ 4.2 If_Else.py
@@ -0,0 +1,23 @@
+color = "blue"
+if color == "blue":
+    print("your color is blue")
+
+if color != "blue":
+    print("color ain't blue")
+
+
+grade = 89
+if grade > 50:
+    print("you passed")
+
+else:
+    print("I am sorry, try again")
+
+
+boy = "good"
+if boy == "good":
+    print("he is on santa's good list")
+
+else:
+    print("the boy is on the wrong list")
+
